ef8fe957a975fd324ca1f326a00ff992c1596ea3
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-08-07 12:00:26 +0330

    First Model Named Article - First Http Response - 0002

--- news_web/news_app/models.py
+++ news_web/news_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    CHOICES = (
+        ('p', 'published'),
+        ('d', 'draft'),
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=CHOICES, max_length=1)
